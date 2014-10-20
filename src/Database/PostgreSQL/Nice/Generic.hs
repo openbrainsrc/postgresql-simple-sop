290b14668193d68007a1505cb3811eebe32de7b9
Tom Nielsen <tanielsen@gmail.com>
2014-10-20 16:17:06 +0100

    gselect

--- src/Database/PostgreSQL/Nice/Generic.hs
+++ src/Database/PostgreSQL/Nice/Generic.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE OverloadedStrings, ScopedTypeVariables, DeriveGeneric, FlexibleInstances, ConstraintKinds, DataKinds, GADTs #-}
 
-module Database.PostgreSQL.Nice.Generic (gfromRow, gtoRow) where
+module Database.PostgreSQL.Nice.Generic (gfromRow, gtoRow, gselect) where
 
 import Generics.SOP
-import qualified GHC.Generics as GHC
 import Control.Applicative
+import Data.Monoid ((<>))
+import Data.List (intercalate)
+import Data.String (fromString)
 
 import Database.PostgreSQL.Simple
 import Database.PostgreSQL.Simple.FromRow
@@ -13,15 +15,11 @@ import Database.PostgreSQL.Simple.ToField
 
 -- https://ocharles.org.uk/blog/posts/2014-08-07-postgresql- simple-generic-sop.html
 
-fields :: (All FromField xs, SingI xs) => RowParser (NP I xs)
-fields = hsequence (hcpure fromField field)
-  where fromField = Proxy :: Proxy FromField
-
 gfromRow
   :: (All FromField xs, Code a ~ '[xs], SingI xs, Generic a)
   => RowParser a
-gfromRow = to . SOP . Z <$> hsequence (hcpure fromField field)
-  where fromField = Proxy :: Proxy FromField
+gfromRow = to . SOP . Z <$> hsequence (hcpure fromFieldp field)
+  where fromFieldp = Proxy :: Proxy FromField
 
 gtoRow :: (Generic a, Code a ~ '[xs], All ToField xs, SingI xs) => a -> [Action]
 gtoRow a =
@@ -29,3 +27,20 @@ gtoRow a =
     SOP (Z xs) -> hcollapse (hcliftA toFieldP (K . toField . unI) xs)
 
   where toFieldP = Proxy :: Proxy ToField
+
+fieldNames :: forall a. (Generic a, HasDatatypeInfo a) => Proxy a -> [String]
+fieldNames _ = case datatypeInfo (Proxy :: Proxy a) of
+  ADT     _ _ cs -> fNms cs
+  Newtype _ _ c -> fNms $ c :* Nil
+
+fNms :: NP ConstructorInfo a -> [String]
+fNms ((Record _ fs) :* _) = fNmsRec fs
+
+fNmsRec :: NP FieldInfo a -> [String]
+fNmsRec Nil = []
+fNmsRec (FieldInfo nm :* rest) = nm : fNmsRec rest
+
+-- gselect conn "from persons where name = ? " theName
+
+gselect :: forall r q. (ToRow q, FromRow r, Generic r, HasDatatypeInfo r) => Connection -> Query -> q -> IO [r]
+gselect conn q1 args = query conn ("select (" <> (fromString $ intercalate "," $ fieldNames $ (Proxy :: Proxy r) ) <> ") " <> q1) args
